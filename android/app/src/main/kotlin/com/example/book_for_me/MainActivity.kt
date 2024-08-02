0d1dd85ac9185b6dc21b5cc34cb27ade8dc4c1a8
Youssef Mohamed <joe375649@gmail.com>
2024-08-02 14:55:00 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/book_for_me/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.book_for_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -0,0 +1,10 @@
+
+import 'package:book_for_me/Features/home/domain/entities/book_entity.dart';
+import 'package:book_for_me/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks(
+      {int pageNumber = 0});
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
+}
